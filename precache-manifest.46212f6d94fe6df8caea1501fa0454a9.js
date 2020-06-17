d6c6cdf47f5dcc633bcb08a46efbb868953b5dc0
Parsa Hejabi <parsa.hejabi@gmail.com>
2020-06-18 03:09:14 +0430

    Updates

---
+++ precache-manifest.46212f6d94fe6df8caea1501fa0454a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c51f7c2ee8fc5acb018bfe5253c281",
+    "url": "/TweetCleaner/index.html"
+  },
+  {
+    "revision": "709c9f56a4b19eae805b",
+    "url": "/TweetCleaner/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2dfc00ed2207b22e4b8b",
+    "url": "/TweetCleaner/static/js/2.aeb31073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TweetCleaner/static/js/2.aeb31073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709c9f56a4b19eae805b",
+    "url": "/TweetCleaner/static/js/main.3e2a064a.chunk.js"
+  },
+  {
+    "revision": "361ffedd75e3e3e0c1ab",
+    "url": "/TweetCleaner/static/js/runtime-main.b18f7993.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TweetCleaner/static/media/logo.5d5d9eef.svg"
+  }
+]);
